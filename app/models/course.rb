02a21c129a14ad8f4be8358b9f885d75978914ec
Ian de Jongh <iandagong@gmail.com>
2023-09-05 00:08:00 +1000

    remove old xml code

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,20 +2,6 @@ class Course < ApplicationRecord
   belongs_to :event
   has_many :results, dependent: :destroy
 
-  def self.create_from_hash(event, course_hash, race_lookup)
-    @course = Course.find_or_create_by(
-      event: event,
-      name: course_hash.dig("Class", "Name")&.split(":")&.first,
-      distance: course_hash.dig("Course", "Length")
-    )
-    results = course_hash["PersonResult"]
-    results = [results] if results.is_a? Hash
-    results.each do |result|
-      Result.create_from_hash(@course, result, race_lookup)
-    end
-    @course
-  end
-
   def control_sequence(race_no)
     results.race_number(race_no).each do |result|
       splits = result.splits["splits"]
--- app/models/race.rb
+++ app/models/race.rb
@@ -2,11 +2,4 @@ class Race < ApplicationRecord
   belongs_to :event
   has_many :results
 
-  def self.create_from_hash(event, race_hash)
-    @race = Race.create(
-      event: event,
-      number: race_hash.dig("RaceNumber"),
-      name: race_hash.dig("Name")&.strip
-    )
-  end
 end
